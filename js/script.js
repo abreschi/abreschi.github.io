56171a3181876af3795add3051d525aec008e8d9
abreschi <ale.breschi@gmail.com>
2017-05-22 13:54:15 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-function myFunction() {
+$("nav .icon").click(function() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
       x.className += " responsive";
   } else {
       x.className = "topnav";
   }
-}
+});
 
+
+var $navli = $("nav li");
