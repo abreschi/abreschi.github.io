cf0d7dabaf393c039e7eda80273165b2eaa16a99
abreschi <ale.breschi@gmail.com>
2017-07-04 19:40:08 +0200

    Add javascript to populate skill list. Now need to write the skills object

--- js/script.js
+++ js/script.js
@@ -20,3 +20,28 @@ $navli.children().each( function() {
 		$(this).addClass("selected");
 	};
 });
+
+/******************
+  SKILL BARS
+*******************/
+
+// List of skills with labels and scores
+var skills = {
+  html : {"label": "HTML & CSS", "score": 0.8},
+  js : {"label": "JavaScript", "score": 0.3},
+};
+
+// Iterate over skills and add bars
+var skillBarTotalWidth = 200;
+for (var el in skills) {
+  // Define score wrt total bar width
+  var skillBarWidth = skills[el].score * skillBarTotalWidth;
+  // Create internal bar --> represent the score of the skill
+  var $skillBarSpan = $('<span></span>').addClass('skillBar').css("width", skillBarWidth+"px");
+  // Create background bar for skill and append the score bar
+  var $skillBar100Span = $('<span></span>').addClass('skillBar100').append($skillBarSpan);
+  // Create the skill label
+  var $skillLabel = $('<label></label>').text(skills[el].label);
+  // Append all the previous elements to the skills ul
+  $('#skills-body ul').append($('<li></li>').append($skillLabel).append($skillBar100Span));
+};
